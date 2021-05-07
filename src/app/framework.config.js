b7e0de271ccf47a562dcd73f8e8c13bf1a2b18d0
Ashwin Date <coolbung@gmail.com>
2021-05-07 19:22:44 +0530

    Update framework.config.js

--- src/app/framework.config.js
+++ src/app/framework.config.js
@@ -12,6 +12,7 @@ module.exports = {
     db: {
         cassandra: {
         contactPoints: envHelper.PORTAL_CASSANDRA_URLS,
+        localDataCenter: 'datacenter1',
         defaultKeyspaceSettings: {
             replication: replicationStrategy,
         },
